1f9dd03bd68be094c9f096217c818234697b5015
Fizzor <36672655+Fizzor96@users.noreply.github.com>
2021-12-06 00:44:23 +0100

    imgui include

--- program/src/main.cpp
+++ program/src/main.cpp
@@ -1,13 +1,17 @@
 #include "Engine.h"
+#include "imgui-SFML.h"
+#include "imgui.h"
 #include <SFML/Graphics.hpp>
 #include <math.h>
+#include <string>
 
 using namespace std;
 
 int main()
 {
     eke::Globals::Init();
-    // eke::Controller::Init();
+    ImGui::SFML::Init(*eke::Globals::RenderWindow);
+    eke::Controller::Init();
 
     while (eke::Globals::RenderWindow->isOpen())
     {
@@ -15,15 +19,27 @@ int main()
 
         while (eke::Globals::RenderWindow->pollEvent(*eke::Globals::Event))
         {
+            ImGui::SFML::ProcessEvent(*eke::Globals::RenderWindow, *eke::Globals::Event);
             eke::Globals::PollEvents();
-            // eke::Controller::PollEvents();
+            eke::Controller::PollEvents();
         }
 
+        ImGui::SFML::Update(*eke::Globals::RenderWindow, eke::Globals::Clock->restart());
+        ImGui::ShowDemoWindow();
+        ImGui::Begin("Hello, world!");
+        if (ImGui::Button("Look at this pretty button"))
+        {
+            std::cout << "Szia Pence!\n"
+                      << std::endl;
+        }
+        ImGui::End();
+
         eke::Globals::Update();
-        // eke::Controller::Update();
+        eke::Controller::Update();
 
         eke::Globals::RenderWindow->clear(sf::Color(54, 49, 60, 255));
-        // eke::Controller::Draw();
+        eke::Controller::Draw();
+        ImGui::SFML::Render(*eke::Globals::RenderWindow);
         eke::Globals::RenderWindow->display();
     }
 
